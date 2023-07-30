a6182b7f70eee88cb48ae8081f028ec06e3c7f3a
luizotavio.delgado@gmail.com <luizotavio.delgado@gmail.com>
2023-07-30 01:35:17 +0000

    Ajuste de quantidade na fact_orders

--- models/mart/fct_orders.sql
+++ models/mart/fct_orders.sql
@@ -38,6 +38,14 @@ with
         from {{  ref('dim_countryregions')  }}
     )
 
+    , quantity as (
+        select
+            sales_salesorderdetail.salesorderid
+            , sum(sales_salesorderdetail.orderqty) as quantity
+        from {{  ref('stg_sap__salesorderdetail')  }} sales_salesorderdetail
+        group by 1
+    )
+
     , sales_salesorderheader_with_fk as (
         select
             sales_salesorderheader.salesorderid
@@ -61,6 +69,7 @@ with
             , sales_creditcard_with_sk.creditcardid_sk as creditcardid_fk
             , sales_salesorderheader.creditcardapprovalcode
             , sales_salesorderheader.currencyrateid
+            , quantity.quantity
             , sales_salesorderheader.subtotal
             , sales_salesorderheader.taxamt
             , sales_salesorderheader.freight
@@ -77,6 +86,8 @@ with
         on person_address_with_sk.stateprovinceid = person_stateprovince_with_sk.stateprovinceid
         left join person_countryregion_with_sk
         on person_stateprovince_with_sk.countryregioncode = person_countryregion_with_sk.countryregioncode
+        left join quantity
+        on sales_salesorderheader.salesorderid = quantity.salesorderid
     )
 
     select * from sales_salesorderheader_with_fk
